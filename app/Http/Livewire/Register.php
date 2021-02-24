a1bbf82b6d09c98ad24f625b8782d12895d91354
brokoli <brokoli@marketboro.com>
2021-02-25 08:51:16 +0900

    라이브와이어 폼 바인딩 해야함

--- app/Http/Livewire/Register.php
+++ app/Http/Livewire/Register.php
@@ -2,10 +2,46 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Tenant;
 use Livewire\Component;
 
 class Register extends Component
 {
+    public $name = '';
+    public $companyName = '';
+    public $email = '';
+    public $password = '';
+
+    public function register()
+    {
+        dd('here');
+        $this->validate([
+            'name' => ['required', 'string'],
+            'companyName' => ['required', 'string', 'unique:tenants,name'],
+            'email' => ['required', 'email', 'unique:users'],
+            'password' => ['required', 'min:8'],
+        ]);
+
+        $tenant = Tenant::create([
+            'name' => $this->companyName,
+        ]);
+
+        $user = User::create([
+            'email' => $this->email,
+            'name' => $this->name,
+            'role' => 'admin',
+            'password' => Hash::make($this->password),
+            'tenant_id' => $tenant->id,
+        ]);
+
+        $user->sendEmailVerificationNotification();
+
+        Auth::login($user, true);
+
+        redirect(route('home'));
+    }
+
+
     public function render()
     {
         return view('livewire.register');
